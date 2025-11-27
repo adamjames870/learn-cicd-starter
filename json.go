c90e8401b60363159b73730f7315b2ccf3438be6
Adam James <adam.james870@yahoo.co.uk>
2025-11-27 05:58:45 +0000

    Fixes gosec identified vulnerabilities

--- json.go
+++ json.go
@@ -30,5 +30,10 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, errWrite := w.Write(dat)
+	if errWrite != nil {
+		log.Printf("Error writing JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
 }
